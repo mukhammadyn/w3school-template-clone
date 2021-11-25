eafa6f6bf0663aa530eb22da2588d06c60753b9e
Mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2021-11-25 10:53:50 +0500

    Add navigation

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// site nav buttons
+const elsNavButton = document.querySelectorAll('.js-nav-button')
+// site nav
+const elSiteNav = document.querySelector('.site-nav')
+
+function siteNavOpenerAndCloser (evt) {
+
+  console.log(evt.target.matches('.js-nav-button-closer')); 
+
+  if (evt.target.matches('.js-nav-button-closer') && elSiteNav.matches('.site-nav--open')) {
+    elSiteNav.classList.remove('site-nav--open')
+  } else {
+    elSiteNav.classList.add('site-nav--open')
+  }
+
+}
+
+if (elsNavButton.length > 0) {
+
+  elsNavButton.forEach(btn => {
+    btn.addEventListener('click', siteNavOpenerAndCloser)
+  })
+
+}
